f3fab0275d9632907f49a50791cc747be709bb81
carlosnascimentosouza20-ops <carlos.nascimento.souza20@escola.pr.gov.br>
2026-06-01 09:51:41 -0300

    tudo feito

---
+++ style.cs
@@ -0,0 +1,75 @@
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, sans-serif;
+}
+
+body{
+    background-color: #e6f7ff;
+    color: #333;
+    line-height: 1.6;
+}
+
+header{
+    background: #87cefa;
+    color: white;
+    text-align: center;
+    padding: 40px;
+}
+
+header h1{
+    font-size: 2.8rem;
+}
+
+header p{
+    font-size: 1.2rem;
+}
+
+nav{
+    background: #5dade2;
+    text-align: center;
+    padding: 15px;
+}
+
+nav a{
+    color: white;
+    text-decoration: none;
+    margin: 15px;
+    font-weight: bold;
+}
+
+nav a:hover{
+    color: #eafaf1;
+}
+
+main{
+    width: 85%;
+    margin: 20px auto;
+}
+
+section{
+    background: white;
+    margin-bottom: 20px;
+    padding: 25px;
+    border-radius: 12px;
+    box-shadow: 0px 2px 8px rgba(0,0,0,0.1);
+}
+
+section h2{
+    color: #3498db;
+    margin-bottom: 15px;
+}
+
+ul{
+    margin-top: 10px;
+    margin-left: 25px;
+}
+
+footer{
+    background: #87cefa;
+    color: white;
+    text-align: center;
+    padding: 20px;
+    margin-top: 20px;
+}
